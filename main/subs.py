515a78f49f32d6a5dc41899d293ad7283132cfe7
Fazalika Hismawan <hismawan.fazalika@gmail.com>
2024-12-21 10:59:42 +0700

    Finding a Motif in DNA - Create a function to separate DNA substring from its DNA string - Create a function to locate the position of a motif

---
+++ main/subs.py
@@ -0,0 +1,19 @@
+def separate_two_dna_strings(file_path: str) -> tuple[str, str]:
+    with open(file_path, "r") as text:
+        dna_string, dna_substring = text.read().split("\n")
+
+    return dna_string.strip(), dna_substring.strip()
+
+
+def locate_motif_starting_position(dna_string: str, pattern: str):
+    starting_position = []
+    for i in range(len(dna_string)):
+        if dna_string[i:].startswith(pattern):
+            starting_position.append(i + 1)
+
+    return starting_position
+
+
+dna_string, dna_substring = separate_two_dna_strings("../data/subs_case_1.txt")
+
+print(locate_motif_starting_position(dna_string, dna_substring))
